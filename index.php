d8b33e1ba504be76c78f18d1e4b276d2e4992242
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-02-12 19:10:56 +0500

    switch

--- index.php
+++ index.php
@@ -1,46 +1,79 @@
 <?php
 
-$user_id = 12345;
-// Задаем нужные параметры API Telegram.
-$token = '6146306512:AAGVdpKHJ-VGyu1D2Oc1q6T8xl4nMN1WTrg';
-$chat_id = 'YOUR-CHAT-ID';
-// Формируем URL с API Telegram.
-$url = "https://api.telegram.org/bot". $token ."/getChatMembersCount?chat_id=". $chat_id;
-// Отправляем GET-запрос API Telegram.
-$result = file_get_contents($url);
-// Получаем JSON-ответ API Telegram.
-$result = json_decode($result);
-// Получаем список ID участников.
-$members = $result->result;
-// Проверяем, является ли ID юзера, чью роль мы хотим проверить, участником канала.  
-if (in_array($user_id, $members)) {  
-    echo 'User is a member of the channel.';  
-} else {  
-    echo 'User is not a member of the channel.';  
+
+include 'Telegram.php';
+
+$telegram = new Telegram('6146306512:AAGVdpKHJ-VGyu1D2Oc1q6T8xl4nMN1WTrg');
+$chat_id = $telegram->ChatID();
+$text=$telegram->Text();
+
+$orderTypes=["1 kg - 100000" , "2 kg - 200000" ,"3 kg - 300000","4 kg - 400000"];
+
+switch($text){
+    case "/start":
+        showstart();
+        break;
+    case "Batafsil ma\'lumot":
+        showabout();
+        break;
+    case "Zakaz berish";
+        showOrder();
+        break;
+    default: 
+        if(in_array($text, $orderTypes)){
+        showAsk();
+        }
+        break;
+    
 }
 
-// include 'Telegram.php';
 
-// $telegram = new Telegram('6146306512:AAGVdpKHJ-VGyu1D2Oc1q6T8xl4nMN1WTrg');
+function showstart(){
+      global $telegram ,$chat_id;
 
-// $chat_id = $telegram->ChatID();
-// $text=$telegram->Text();
+    $option = [ 
+        array($telegram->buildKeyboardButton("Batafsil ma'lumot")),
+       array($telegram->buildKeyboardButton("Zakaz berish"))
+    ];
+    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Assalomu alaykum bot xush kelib siz");
+    $telegram->sendMessage($content);
+}
+function  showabout(){
+    global $telegram, $chat_id;
+    $option = [
+        array($telegram->buildKeyboardButton("Saytga ulaning"))
+    ];
 
-// if($text == '/start'){
+    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "ma'lumotlarni quyidagi saytdan olishingiz mumkin lochinbek.uz");
+    $telegram->sendMessage($content);
+}
+function showOrder(){
 
-    // $option = array( 
-        //First row
-        // array($telegram->buildKeyboardButton("Batafsil"), $telegram->buildKeyboardButton("Zakaz berish")));
-        //Second row 
-        
-        
-    // $keyb = $telegram->buildKeyBoard($option, $onetime=true,$resize=true);
-    // $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Assalomu alaykum. Botimizga xush kelibsiz! Bizning kanalimizga a'zo");
-    // $telegram->sendMessage($content);
+    global $telegram , $chat_id;
 
-    
-    // }
+    $option = [
+        array($telegram->buildKeyboardButton("1 kg - 100000"), $telegram->buildKeyboardButton("2 kg - 200000")), 
+        array($telegram->buildKeyboardButton("3 kg - 300000"), $telegram->buildKeyboardButton("4 kg - 400000")), 
+    ];
+    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Qancha zakaz berasiz hurmatli mijoz");
+    $telegram->sendMessage($content);
+}
+function showAsk(){
+    global $telegram , $chat_id;
 
+    $option = array( 
+        array($telegram->buildKeyboardButton("Raqamni yuborish" , $request_contact= true)), 
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "hajm tanlandi Bog'lanish uchu raqamingiz yuboring");
+    $telegram->sendMessage($content);
+}
+
+
+// elseif
 // elseif($text =='Batafsil'){
 
 //     $option = array( 
