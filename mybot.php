038f18fc262cfbc6fbd0a3467ca94fa15db3cdd2
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-02-15 16:55:10 +0500

    user bot page

--- mybot.php
+++ mybot.php
@@ -4,6 +4,8 @@ require_once 'Telegram.php';
 
 require_once 'user.php';
 
+require_once 'db_connect.php';
+
 $telegram = new Telegram('6146306512:AAGVdpKHJ-VGyu1D2Oc1q6T8xl4nMN1WTrg');
 
 $data = $telegram->getData();
@@ -14,6 +16,8 @@ $message= $data['message'];
 $chat_id = $message['chat']['id'];
 
 $text=$message['text'];
+
+$orderTypes=["1 kg - 100000" , "2 kg - 200000" ,"3 kg - 300000","4 kg - 400000"];
 
 if($text == '/start'){
        showMain();
@@ -28,6 +32,25 @@ if($text == '/start'){
                 chooseButton();
             }
             break;
+        case 'massa':
+            if(in_array($text, $orderTypes)){
+                setMassa($chat_id, $text);
+                // showAsk();
+            }elseif($text == 'back'){
+                showMain();
+            }else{
+                chooseButton();
+            }
+            break;
+        case 'phone':
+                if($message['contact']['phone_number'] != ''){
+                    file_put_contents('users/phone.txt', $message['contact']['phone_number']);
+                }elseif($text == 'back'){
+                    // showOrder();
+                }else{
+                    chooseButton();
+                }
+                break;
     }
 }
 
@@ -50,5 +73,37 @@ function chooseButton(){
     global $telegram ,$chat_id;
     
     $content = ['chat_id'=>$chat_id, 'text'=> 'iltimos quyidagi tugmalardan birini tanlang'];
+    $telegram->sendMessage($content);
+}
+
+
+function showOrder(){
+
+    global $telegram , $chat_id;
+
+    setMassa($chat_id, 'massa');
+
+    $option = [
+        array($telegram->buildKeyboardButton("1 kg - 100000"), $telegram->buildKeyboardButton("2 kg - 200000")), 
+        array($telegram->buildKeyboardButton("3 kg - 300000"), $telegram->buildKeyboardButton("4 kg - 400000")),
+        array($telegram->buildKeyboardButton("back")),
+        
+    ];
+    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Qancha zakaz berasiz hurmatli mijoz");
+    $telegram->sendMessage($content);
+}
+
+
+function showAsk(){
+    global $telegram , $chat_id ,$message;
+  
+    setMassa($chat_id, 'massa');
+
+    $option = array( 
+        array($telegram->buildKeyboardButton("Raqamni yuborish" , $request_contact= true)), 
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Hajm tanlandi bog'lanish uchun raqamingiz yuboring");
     $telegram->sendMessage($content);
 }
--- user.php
+++ user.php
@@ -7,3 +7,19 @@ function setPage($chat_id, $page){
 function getPage($chat_id){
     return file_get_contents('users/'.$chat_id. 'page.txt');
 }
+
+function setMassa($chat_id, $page){
+    file_put_contents('users/'.$chat_id. 'massa.txt', $page);
+}
+
+function getMassa($chat_id){
+    return file_get_contents('users/'.$chat_id. 'massa.txt');
+}
+
+function setPhone($chat_id, $page){
+    file_put_contents('users/'.$chat_id. 'phone.txt', $page);
+}
+
+function getPhone($chat_id){
+    return file_get_contents('users/'.$chat_id. 'phone.txt');
+}
