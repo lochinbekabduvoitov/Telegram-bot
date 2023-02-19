24ba863a2645d5dfc81a7c3278ee52db1d82ecab
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-02-19 10:20:53 +0500

    success project

--- mybot.php
+++ mybot.php
@@ -1,109 +1,223 @@
 <?php
 
 require_once 'Telegram.php';
-
 require_once 'user.php';
-
-require_once 'db_connect.php';
 
 $telegram = new Telegram('6146306512:AAGVdpKHJ-VGyu1D2Oc1q6T8xl4nMN1WTrg');
 
+$ADMIN_CHAT_ID = 635793263;
+
 $data = $telegram->getData();
-$message= $data['message'];
 
+$message = $data['message'];
 
+//sendMessageJson();
 
+$text = $message['text'];
 $chat_id = $message['chat']['id'];
+$firstName = $message['from']['first_name'];
+$lastName = $message['from']['last_name'];
 
-$text=$message['text'];
+// order buttons texts
+$orderTypes = ["1kg - 50 000 so'm", "1.5kg (1L) - 75 000 so'm", "4.5kg (3L) - 220 000 so'm", "7.5kg (5L) - 370 000 so'm"];
 
-$orderTypes=["1 kg - 100000" , "2 kg - 200000" ,"3 kg - 300000","4 kg - 400000"];
-
-if($text == '/start'){
-       showMain();
-}else{
-    switch(getPage($chat_id)){
-        case 'main';
-            if($text == "Batafsil ma'lumot"){
-                showabout();
-            }elseif($text == "Zakaz berish"){
-                showOrder();
-            }else{
-                chooseButton();
+if ($text == "/start") {
+    showMain();
+} else {
+    switch (getPage($chat_id)) { // check current page
+        case 'main':
+            if ($text == "🍯 Batafsil ma'lumot") {
+                showAbout();
+            } elseif ($text == "🍯 Buyurtma berish") {
+                showMass();
+            } else {
+                chooseButtons();
             }
             break;
         case 'massa':
-            if(in_array($text, $orderTypes)){
-                setMassa($chat_id, $text);
-                // showAsk();
-            }elseif($text == 'back'){
+            if (in_array($text, $orderTypes)) {
+                setMass($chat_id, $text);
+                showPhone();
+            } elseif ($text == "🔙 Orqaga") {
                 showMain();
-            }else{
-                chooseButton();
+            } else {
+                chooseButtons();
             }
             break;
         case 'phone':
-                if($message['contact']['phone_number'] != ''){
-                    file_put_contents('users/phone.txt', $message['contact']['phone_number']);
-                }elseif($text == 'back'){
-                    // showOrder();
-                }else{
-                    chooseButton();
-                }
-                break;
+            if ($message['contact']['phone_number'] != "") {
+                setPhone($chat_id, $message['contact']['phone_number']);
+                showDeliveryType();
+            } elseif ($text == "🔙 Orqaga") {
+                showMass();
+            } else {
+                setPhone($chat_id, $text);
+                showDeliveryType();
+            }
+            break;
+        case 'delivery':
+            if ($text == "✈️ Yetkazib berish ✈") {
+                showInputLocation();
+            } elseif ($text == "🍯 Borib olish 🍯") {
+                showReady();
+            } elseif ($text == "🔙 Orqaga") {
+                showPhone();
+            } else {
+                chooseButtons();
+            }
+            break;
+        case 'location':
+            if ($message['location']['latitude'] != "") {
+                setLatitude($chat_id, $message['location']['latitude']);
+                setLongitude($chat_id, $message['location']['latitude']);
+                showReady();
+            } elseif ($text == "Lokatsiya jo'nata olmayman") {
+                showReady();
+            } elseif ($text == "🔙 Orqaga") {
+                showDeliveryType();
+            } else {
+                chooseButtons();
+            }
+            break;
+        case 'ready':
+            if ($text == "Boshqa buyurtma berish") {
+                showMain();
+            } else {
+                chooseButtons();
+            }
+            break;
     }
 }
 
-function showMain(){
-    global $telegram ,$chat_id,$filePath; 
+function showMain()
+{
+    global $telegram, $chat_id, $firstName, $lastName;
 
     setPage($chat_id, 'main');
 
-  $option = [ 
-      array($telegram->buildKeyboardButton("Batafsil ma'lumot")),
-     array($telegram->buildKeyboardButton("Zakaz berish"))
-  ];
-  $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
-  $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Assalomu alaykum!");
-  $telegram->sendMessage($content);
-}
-
-function chooseButton(){
-
-    global $telegram ,$chat_id;
-    
-    $content = ['chat_id'=>$chat_id, 'text'=> 'iltimos quyidagi tugmalardan birini tanlang'];
-    $telegram->sendMessage($content);
-}
-
-
-function showOrder(){
-
-    global $telegram , $chat_id;
-
-    setMassa($chat_id, 'massa');
-
-    $option = [
-        array($telegram->buildKeyboardButton("1 kg - 100000"), $telegram->buildKeyboardButton("2 kg - 200000")), 
-        array($telegram->buildKeyboardButton("3 kg - 300000"), $telegram->buildKeyboardButton("4 kg - 400000")),
-        array($telegram->buildKeyboardButton("back")),
-        
-    ];
-    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
-    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Qancha zakaz berasiz hurmatli mijoz");
-    $telegram->sendMessage($content);
-}
-
-
-function showAsk(){
-    global $telegram , $chat_id ,$message;
-  
-    setMassa($chat_id, 'massa');
-
-    $option = array( 
-        array($telegram->buildKeyboardButton("Raqamni yuborish" , $request_contact= true)), 
+    $option = array(
+        //First row
+        array($telegram->buildKeyboardButton("🍯 Batafsil ma'lumot")),
+        //Second row
+        array($telegram->buildKeyboardButton("🍯 Buyurtma berish"))
     );
-    $keyb = $telegram->buildKeyBoard($option, $onetime=true, $resize=true);
-    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Hajm tanlandi bog'lanish uchun raqamingiz yuboring");
+    $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
+    $content = array('chat_id' => $chat_id, 'text' => "Assalom alaykum, {$firstName} {$lastName}!
+Ushbu bot orqali siz BeeO asal-arichilik firmasidan tabiiy asal va  asal mahsulotlarini sotib olishingiz mumkin!");
+    $telegram->sendMessage($content);
+    $content = array('chat_id' => $chat_id, 'disable_web_page_preview' => false, 'reply_markup' => $keyb, 'text' => "Mening ismim Jamshid, ko`p yillardan beri oilaviy arichilik bilan shug`illanib kelamiz!
+BeeO -asalchilik firmamiz mana 3 yildirki, Toshkent shahri aholisiga toza, tabiiy asal yetkizib bermoqda va ko`plab xaridorlarga ega bo`ldik, shukurki, shu yil ham arichiligimizni biroz kengaytirib siz azizlarning ham dasturxoningizga tabiiy-toza asal yetkazib berishni niyat qildik!");
+    $telegram->sendMessage($content);
+}
+
+function showMass()
+{
+    global $telegram, $chat_id, $filePath;
+
+    setPage($chat_id, 'massa');
+
+    $option = array(
+        array($telegram->buildKeyboardButton("1kg - 50 000 so'm")),
+        array($telegram->buildKeyboardButton("1.5kg (1L) - 75 000 so'm")),
+        array($telegram->buildKeyboardButton("4.5kg (3L) - 220 000 so'm")),
+        array($telegram->buildKeyboardButton("7.5kg (5L) - 370 000 so'm")),
+        array($telegram->buildKeyboardButton("🔙 Orqaga")),
+
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Buyurtma berish uchun hajmlardan birini tanlang yoki o'zingiz hohlagan hajmni kiriting.");
+    $telegram->sendMessage($content);
+}
+
+function showPhone()
+{
+    global $telegram, $chat_id;
+
+    setPage($chat_id, 'phone');
+
+    $option = array(
+        array($telegram->buildKeyboardButton("Raqamni jo'natish", $request_contact = true)),
+        array($telegram->buildKeyboardButton("🔙 Orqaga")),
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'Hajm tanlandi, endi telefon raqamingnizni kiritsangiz.');
+    $telegram->sendMessage($content);
+}
+
+function showDeliveryType()
+{
+    global $telegram, $chat_id;
+
+    setPage($chat_id, 'delivery');
+
+    $option = array(
+        array($telegram->buildKeyboardButton("✈️ Yetkazib berish ✈")),
+        array($telegram->buildKeyboardButton("🍯 Borib olish 🍯")),
+        array($telegram->buildKeyboardButton("🔙 Orqaga")),
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Bizda Toshkent shahri bo'ylab yetkazib berish xizmati mavjud. Yoki, o'zingiz tashrif buyurib olib ketishingiz mumkin!
+Manzil: Toshkent sh, Olmazor tum. Talabalar shaharchasi.");
+    $telegram->sendMessage($content);
+}
+
+function showInputLocation()
+{
+    global $telegram, $chat_id;
+
+    setPage($chat_id, 'location');
+
+    $option = array(
+        array($telegram->buildKeyboardButton("Lokatsiya jo'natish", false, true)),
+        array($telegram->buildKeyboardButton("Lokatsiya jo'nata olmayman")),
+        array($telegram->buildKeyboardButton("🔙 Orqaga")),
+    );
+    $key = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $key, 'text' => "Yaxshi, endi, lokatsiya jo'nating!");
+    $telegram->sendMessage($content);
+}
+
+function showReady()
+{
+    global $telegram, $chat_id, $ADMIN_CHAT_ID;
+
+    setPage($chat_id, 'ready');
+
+    $option = array(
+        array($telegram->buildKeyboardButton("Boshqa buyurtma berish")),
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'Sizning buyurtmangiz qabul qilindi. Tez orada siz bilan bog\'lanamiz. Murojaatingiz uchun rahmat! 😊');
+    $telegram->sendMessage($content);
+
+    //send admin
+    $text = "Yangi buyurtma keldi!";
+    $text.= "\n";
+    $text.= "Hajm: ".getMass($chat_id);
+    $text.= "\n";
+    $text.= "Telefon raqam: ".getPhone($chat_id);
+    $text.= "\n";
+
+    $content = array('chat_id' => $ADMIN_CHAT_ID, 'reply_markup' => $keyb, 'text' => $text);
+    $telegram->sendMessage($content);
+
+    if (getLatitude($chat_id) != "") {
+        $content = array('chat_id' => $ADMIN_CHAT_ID, 'latitude' => getLatitude($chat_id), 'longitude' => getLongitude($chat_id));
+        $telegram->sendLocation($content);
+    }
+}
+
+function showAbout()
+{
+    global $telegram, $chat_id;
+    $content = array('chat_id' => $chat_id, 'text' => "Biz haqimizda ma'lumot. <a href='https://telegra.ph/Biz-haqimizda-05-12'>Havola</a>", 'parse_mode' => "html");
+    $telegram->sendMessage($content);
+}
+
+function chooseButtons()
+{
+    global $telegram, $chat_id;
+
+    $content = array('chat_id' => $chat_id, 'text' => "Iltimos, quyidagi tugmalardan birini tanlang.");
     $telegram->sendMessage($content);
 }
--- user.php
+++ user.php
@@ -1,25 +1,42 @@
 <?php
 
-function setPage($chat_id, $page){
-    file_put_contents('users/'.$chat_id. 'page.txt', $page);
+function setPage($chat_id, $data)
+{
+    file_put_contents('users/' . $chat_id . 'page.txt', $data);
 }
 
-function getPage($chat_id){
-    return file_get_contents('users/'.$chat_id. 'page.txt');
+function getPage($chat_id) {
+    return file_get_contents('users/' . $chat_id . 'page.txt');
 }
 
-function setMassa($chat_id, $page){
-    file_put_contents('users/'.$chat_id. 'massa.txt', $page);
+function setMass($chat_id, $data) {
+    file_put_contents('users/' . $chat_id . 'mass.txt', $data);
 }
 
-function getMassa($chat_id){
-    return file_get_contents('users/'.$chat_id. 'massa.txt');
+function getMass($chat_id) {
+    return file_get_contents('users/' . $chat_id . 'mass.txt');
 }
 
-function setPhone($chat_id, $page){
-    file_put_contents('users/'.$chat_id. 'phone.txt', $page);
+function setPhone($chat_id, $data) {
+    file_put_contents('users/' . $chat_id . 'phone.txt', $data);
 }
 
-function getPhone($chat_id){
-    return file_get_contents('users/'.$chat_id. 'phone.txt');
+function getPhone($chat_id) {
+    return file_get_contents('users/' . $chat_id . 'phone.txt');
+}
+
+function setLatitude($chat_id, $data) {
+    file_put_contents('users/' . $chat_id . 'latitude.txt', $data);
+}
+
+function getLatitude($chat_id) {
+    return file_get_contents('users/' . $chat_id . 'latitude.txt');
+}
+
+function setLongitude($chat_id, $data) {
+    file_put_contents('users/' . $chat_id . 'longitude.txt', $data);
+}
+
+function getLongitude($chat_id) {
+    return file_get_contents('users/' . $chat_id . 'longitude.txt');
 }
